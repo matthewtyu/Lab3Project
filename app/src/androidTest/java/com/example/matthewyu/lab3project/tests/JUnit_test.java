f90497fe84726e2cd18451ae49392183bbe096bf
MatthewYu <matthew.t.yu@icloud.com>
2016-04-15 11:25:22 -0700

    First commit for Lab3Project

---
+++ app/src/androidTest/java/com/example/matthewyu/lab3project/tests/JUnit_test.java
@@ -0,0 +1,29 @@
+package com.example.matthewyu.lab3project.tests;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import com.example.matthewyu.lab3project.MainActivity;
+import com.example.matthewyu.lab3project.R;
+
+/**
+ * Created by matthewyu on 4/13/16.
+ */
+public class JUnit_test extends ActivityInstrumentationTestCase2<MainActivity>{
+
+    MainActivity mainActivity;
+    public JUnit_test(){
+
+        super(MainActivity.class);
+
+    }
+
+    public void test_first()
+    {
+        mainActivity = getActivity();
+        TextView textView = (TextView) mainActivity.findViewById(R.id.tv1);
+        String tester = textView.getText().toString();
+
+        assertEquals("Bye World", tester);
+
+    }
+}
